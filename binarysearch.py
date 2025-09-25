35e44afda24dbec815c67c6f357cc919dd826c79
Timur <timur63745kul@gmail.com>
2025-09-25 10:50:47 +0900

    binary search fix

--- binarysearch.py
+++ binarysearch.py
@@ -1,13 +1,14 @@
-def solve(nums, target):
-    l = -1
-    r = len(nums)
-    while r - l > 1:
-        m = (l + r) // 2
-        if nums[m] <= target:
-            l = m
-        else:
-            r = m
-    if nums[l] != target:
-        return -1
+nums = [int(e) for e in input().split()]
+target = int(input())
+l = -1
+r = len(nums)
+while r - l > 1:
+    m = (l + r) // 2
+    if nums[m] <= target:
+        l = m
     else:
-        return l
+        r = m
+if nums[l] != target:
+    print(-1)
+else:
+    print(l)
